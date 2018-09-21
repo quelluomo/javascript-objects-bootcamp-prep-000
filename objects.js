38018bdf0594cceffc23fb38b78fa8d91510baf7
Matteo Muscas <thatman.j@gmail.com>
2018-09-21 11:08:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,14 @@
 var playlist = { MinoReitano: "Italia" };
 
 function updatePlaylist(playlist, artistName, songTitle){
+<<<<<<< HEAD
   playlist['Phil Ochs'] = 'Ultimo bicchiere';
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.Slowdive;
+=======
+  playlist[MinoReitano] = 'Italia 2';
+>>>>>>> 96aba7c96f7c5df31626ddbc2a7713a379009782
   return playlist;
 }
